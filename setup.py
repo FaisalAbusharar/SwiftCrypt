4d962c0b26a401fae71885c9cc380a29eea9b818
Voidy Devleoper <voidycodes@gmail.com>
2023-08-27 20:38:15 +0300

    changed structure

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Swiftly Secure your Apps'
+LONG_DESCRIPTION = 'Secure your python applications swiftly using SwiftCrypt'
+
+# Setting up
+setup(
+    name="swiftcrypt",
+    version=VERSION,
+    author="Tech Tweaks)",
+    author_email="tech.tweaks.contact@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['keys', 'passwords', 'crypting', 'encoding', 'secure', 'uuid'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
