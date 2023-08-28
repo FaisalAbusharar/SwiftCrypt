d934c30853948dc452300d715e04718cb329c499
Voidy Devleoper <voidycodes@gmail.com>
2023-08-29 01:37:22 +0300

    Finally, actually importing the module works!

---
+++ swiftcrypt/__init__.py
@@ -0,0 +1,5 @@
+from .swiftcrypt import SecretGenerator, DataTransformer, Checker, Hash, TwoFactorAuth, DataMasking, AdvancedFileTransform, RateLimiter, Salts, DigitalSignature
+# Import other classes and functions here
+
+# You can also define what gets imported when using 'from swiftcrypt import *'
+__all__ = ["SecretGenerator", "DataTransformer", "Checker", "Hash", "TwoFactorAuth", "DataMasking", "AdvancedFileTransform", "RateLimiter", "Salts", "DigitalSignature"]
