0b5aae5915b559892118af0ac3efcd229eb4a533
Voidy Devleoper <voidycodes@gmail.com>
2023-08-29 00:40:33 +0300

    dependency fixed
    after 3000 years

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1.4'
+VERSION = '0.1.4.1'
 DESCRIPTION = 'Swiftly Secure your Apps'
 LONG_DESCRIPTION = 'Secure your python applications swiftly using SwiftCrypt'
 
@@ -21,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=["bcrypt==0.3.1", "pyotp==2.9.0", "qrcode==7.4.2", "cryptography==41.0.3"],
+    install_requires=["bcrypt==4.0.1", "pyotp==2.9.0", "qrcode==7.4.2", "cryptography==41.0.3"],
     keywords=['keys', 'passwords', 'crypting', 'encoding', 'secure', 'uuid'],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
