1b78c99b8701fc1eacafeb0050b659c5b7752a4c
Voidy Devleoper <voidycodes@gmail.com>
2023-08-29 21:39:00 +0300

    included classes in __init__.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.3.5.10'
+VERSION = '0.3.5.11'
 DESCRIPTION = 'Swiftly Secure your Apps'
 LONG_DESCRIPTION = long_description
 
--- swiftcrypt/__init__.py
+++ swiftcrypt/__init__.py
@@ -1,5 +1,16 @@
-from .swiftcrypt import SecretGenerator, DataTransformer, Checker, Hash, TwoFactorAuth, DataMasking, AdvancedFileTransform, RateLimiter, Salts, DigitalSignature, SecureInputHandler
+from .swiftcrypt import (SecretGenerator, DataTransformer,
+Checker, Hash,
+TwoFactorAuth, DataMasking,
+AdvancedFileTransform, RateLimiter,
+Salts, DigitalSignature,
+SecureInputHandler, SecureFileHandler,
+SecureSecretStorage, SecureSessionManager)
 # Import other classes and functions here
 
 # You can also define what gets imported when using 'from swiftcrypt import *'
-__all__ = ["SecretGenerator", "DataTransformer", "Checker", "Hash", "TwoFactorAuth", "DataMasking", "AdvancedFileTransform", "RateLimiter", "Salts", "DigitalSignature", "SecureInputHandler"]
+__all__ = ["SecretGenerator", "DataTransformer", "Checker",
+           "Hash", "TwoFactorAuth", "DataMasking",
+           "AdvancedFileTransform", "RateLimiter",
+           "Salts", "DigitalSignature", "SecureInputHandler",
+           "SecureFileHandler", "SecureSecretStorage",
+           "SecureSessionManager"]
